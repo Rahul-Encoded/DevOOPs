35107df8d3dfc096fa8e86531e15294b53b55c3a
Rahul-Encoded <rahulm03@outlook.com>
2025-05-10 21:33:02 +0530

    Add Terraform configuration files for Azure deployment and update .gitignore

---
+++ Azure_07/backend.tf
@@ -0,0 +1,10 @@
+terraform {
+  backend "azurerm" {
+    # This backend block configures the backend to use Azure Storage Account
+    # The backend block is used to configure the backend for storing the Terraform state file
+    # resource_group_name              = "StorageAccount-ResourceGroup"          # Can be passed via `-backend-config=`"resource_group_name=<resource group name>"` in the `init` command.
+    # storage_account_name             = "abcd1234"                              # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
+    # container_name                   = "tfstate"                               # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
+    # key                              = "prod.terraform.tfstate"                # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.
+  }
+}
---
+++ Azure_07/variables.tf
@@ -0,0 +1,13 @@
+variable "prefix" {
+
+}
+
+variable "location" {
+  description = "The Azure region to deploy resources into"
+}
+
+# variables.tf
+variable "client_id" {}
+variable "client_secret" {}
+variable "tenant_id" {}
+variable "subscription_id" {}
